3285501e91f8cace35defae874dc251272d8e5da
Maciej Dudzinski <elmccd@gmail.com>
2018-04-19 23:14:12 +0200

    some loging

--- api/api.ts
+++ api/api.ts
@@ -8,48 +8,26 @@ const elasticsearch = require('elasticsearch');
 
 const env = require('./env.json');
 const config = require('../config.json');
+const jwt = require('jsonwebtoken');
+const morgan = require('morgan');
 
 const flash = require('connect-flash');
 const session = require('express-session')
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy(function(username, password, done) {
-  console.log(username, password, config);
-    if (username === config.user && password === config.password) {
-      console.log('done ij');
-      return done(null, {
-        user: {
-          username
-        }
-      });
-    }
-
-    return done(null, false, { message: 'Incorrect username or password' });
-  }
-));
 
 const client = new elasticsearch.Client({
   host: env.elasticsearch.host
 });
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
 const app = express();
+
+app.set('superSecret', config.secret); // secret variable
 
 app.use(session({ secret: 'er5cvtybnumiuhytd5fr6t7gy8u4567vbbntyu' }));
 app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(morgan('dev'));
 
 // app.get('/login', (req, res) => {
 //   res.setHeader('Content-Type', 'text/html');
@@ -57,17 +35,74 @@ app.use(bodyParser.json());
 // });
 
 const ensureAuthenticated = function(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
+
+  // check header or url parameters or post parameters for token
+  const token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+      if (err) {
+        return res.json({ ok: false, message: 'Failed to authenticate token.' });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        console.log('decoded', decoded);
+        next();
+      }
+    });
+
+  } else {
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+      ok: false,
+      message: 'No token provided.'
+    });
+
   }
-  res.sendStatus(403);
 };
 
-app.post('/login', passport.authenticate('local'), (req, res) => {
-  res.redirect('/');
+app.post('/api/auth', function(req, res) {
+
+  console.log(req.body);
+
+  if (req.body.email === 'elmccd@gmail.com' && req.body.password === 'maciej') {
+    const payload = {
+      email: req.body.email
+    };
+
+    const token = jwt.sign(payload, app.get('superSecret'), {
+      expiresIn: "24h"
+    });
+
+    // return the information including token as JSON
+    res.json({
+      ok: true,
+      token: token,
+      user: {
+        email: req.body.email,
+        name: 'Jerry'
+      }
+    });
+  } else {
+    res.json({ ok: false, message: 'Authentication failed. User not found.' });
+  }
 });
 
-app.get('/search', ensureAuthenticated, (req, res) => {
+app.get('/api/auth', ensureAuthenticated, function(req, res) {
+  console.log(req.decoded);
+  res.send({
+    ok: true,
+    user: {
+      email: req.decoded.email,
+    }
+  });
+});
+
+app.get('/api/search', ensureAuthenticated, (req, res) => {
   const interval = {
     minute: 'minute',
     hour: 'hour',
