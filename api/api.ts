b51bfe0ff6e9949ca9675c71266e6ec814966a26
Maciej Dudzinski <elmccd@gmail.com>
2018-04-19 21:14:45 +0200

    Some views

--- api/api.ts
+++ api/api.ts
@@ -1,25 +1,73 @@
 import { convertESResponse } from "./responses/convertESResponse";
 import { ESSearchResponse } from "./structures/ESSearchResponse";
 
+const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
 const elasticsearch = require('elasticsearch');
 
-const config = require('./env.json');
+const env = require('./env.json');
+const config = require('../config.json');
 
+const flash = require('connect-flash');
+const session = require('express-session')
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  console.log(username, password, config);
+    if (username === config.user && password === config.password) {
+      console.log('done ij');
+      return done(null, {
+        user: {
+          username
+        }
+      });
+    }
+
+    return done(null, false, { message: 'Incorrect username or password' });
+  }
+));
 
 const client = new elasticsearch.Client({
-  host: config.elasticsearch.host
+  host: env.elasticsearch.host
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
 });
 
 const app = express();
 
+app.use(session({ secret: 'er5cvtybnumiuhytd5fr6t7gy8u4567vbbntyu' }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/login', (req, res) => {
+//   res.setHeader('Content-Type', 'text/html');
+//   res.sendfile(path.join(__dirname, 'views/login.html'));
+// });
 
-app.get('/search', (req, res) => {
+const ensureAuthenticated = function(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.sendStatus(403);
+};
+
+app.post('/login', passport.authenticate('local'), (req, res) => {
+  res.redirect('/');
+});
+
+app.get('/search', ensureAuthenticated, (req, res) => {
   const interval = {
     minute: 'minute',
     hour: 'hour',
@@ -35,44 +83,53 @@ app.get('/search', (req, res) => {
       day: 'd',
   }[req.query.i] || 'd';
 
-  client.search({
-    index: 'measure',
-    size: 0,
-    body: {
-      "query": {
-        "range" : {
-          "date" : {
-            "gte" : `now-${gt}/${round}`,
-            "lte" : `now-${lt}/${round}`
-          }
+  const esQuery = {
+    "query": {
+      "range" : {
+        "date" : {
+          "gte" : `now-${gt}/${round}`,
+          "lte" : `now-${lt}/${round}`
         }
-      },
-      "aggs" : {
-        "measure" : {
-          "date_histogram" : {
-            "field" : "date",
-            "interval" : interval
-          },
-          "aggs" : {
-            "domAvg" : { "avg" : { "field" : "dom" } },
-            "domPercentiles" : { "percentiles" : { "field" : "dom" } },
-            "loadAvg" : { "avg" : { "field" : "load" } },
-            "loadPercentiles" : { "percentiles" : { "field" : "load" } }
-          }
+      }
+    },
+    "aggs" : {
+      "measure" : {
+        "date_histogram" : {
+          "field" : "date",
+          "interval" : interval
+        },
+        "aggs" : {
+          "domAvg" : { "avg" : { "field" : "dom" } },
+          "domPercentiles" : { "percentiles" : { "field" : "dom" } },
+          "loadAvg" : { "avg" : { "field" : "load" } },
+          "loadPercentiles" : { "percentiles" : { "field" : "load" } }
         }
       }
     }
+  };
+
+  client.search({
+    index: 'measure',
+    size: 0,
+    body: esQuery
   }, function (error, response: ESSearchResponse) {
     console.log('error', error);
 
     res.json(convertESResponse(response, {
         interval, gt, lt, round
-    }));
+    }, esQuery));
   });
 
 });
 
-app.listen(config.port, () => console.log(`Perfeto API listening on port ${config.port}!`));
+
+app.get('*', express.static('../dashboard/dist'));
+
+app.get('*', (req, res) => {
+  res.sendfile(path.join(__dirname, '../dashboard/dist/index.html'));
+});
+
+app.listen(env.port, () => console.log(`Perfeto API listening on port ${env.port}!`));
 
 client.ping({
   // ping usually has a 3000ms timeout
--- api/responses/convertESResponse.ts
+++ api/responses/convertESResponse.ts
@@ -1,44 +1,47 @@
-import { get, mapValues } from 'lodash';
-import { ESSearchResponse, SearchResponse } from "../structures/ESSearchResponse";
+import {get, mapValues} from 'lodash';
+import {ESSearchResponse, SearchResponse} from "../structures/ESSearchResponse";
 
 export function processData(data) {
- return data.map(item => {
+  return data.map(item => {
     return {
-        key: item.key,
-        count: item.doc_count,
-        measure: {
-            dom: {
-                avg: ~~item.domAvg.value,
-                percentiles: mapValues(item.domPercentiles.values, v => ~~v)
-            },
-            load: {
-                avg: ~~item.loadAvg.value,
-                percentiles: mapValues(item.loadPercentiles.values, v => ~~v)
-            }
+      key_as_string: item.key_as_string,
+      key: item.key,
+      count: item.doc_count,
+      measure: {
+        dom: {
+          avg: ~~item.domAvg.value,
+          percentiles: mapValues(item.domPercentiles.values, v => ~~v)
+        },
+        load: {
+          avg: ~~item.loadAvg.value,
+          percentiles: mapValues(item.loadPercentiles.values, v => ~~v)
         }
+      }
     }
- });
+  });
 }
 
-export function convertESResponse(response: ESSearchResponse, query = {}): SearchResponse {
+export function convertESResponse(response: ESSearchResponse, query, esQuery): SearchResponse {
 
-    const bucketData = get(response, 'aggregations.measure.buckets');
+  const bucketData = get(response, 'aggregations.measure.buckets');
 
-    if (!bucketData) {
-        return generateErrorResponse(query);
-    }
+  if (!bucketData) {
+    return generateErrorResponse(query);
+  }
 
-    return {
-        query,
-        ok: true,
-        data: processData(bucketData)
-    };
+  return {
+    query,
+    esQuery,
+    raw: bucketData,
+    ok: true,
+    data: processData(bucketData)
+  };
 }
 
 export function generateErrorResponse(query): SearchResponse {
 
-    return {
-        query,
-        error: 'Invalid DB response'
-    };
+  return {
+    query,
+    error: 'Invalid DB response'
+  };
 }
